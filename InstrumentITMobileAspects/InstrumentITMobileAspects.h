4775220ff403983b405e37e05c992098f3531e07
sassama <M.sassano@live.de>
2018-01-02 10:59:45 +0100

    first commit

---
+++ InstrumentITMobileAspects/InstrumentITMobileAspects.h
@@ -0,0 +1,20 @@
+//
+//  InstrumentITMobileAspects.h
+//  InstrumentITMobileAspects
+//
+//  Created by NovaTec on 13.10.17.
+//  Copyright © 2017 NovaTec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <InstrumentITMobileAspects/IITMNativeRessources.h>
+
+//! Project version number for InstrumentITMobileAspects.
+FOUNDATION_EXPORT double InstrumentITMobileAspectsVersionNumber;
+
+//! Project version string for InstrumentITMobileAspects.
+FOUNDATION_EXPORT const unsigned char InstrumentITMobileAspectsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstrumentITMobileAspects/PublicHeader.h>
+
+
